06452db4669c0fd698ff8956dda27d9ad4643092
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2026-01-16 19:00:12 +0800

    feat:文章接口完成数据判断

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -2,21 +2,30 @@ const express = require("express");
 const router = express.Router();
 const { Article } = require("../../models");
 
+// 查询文章列表 /admin/articles
 router.get("/", async function (req, res) {
-  // 筛选条件
-  const condition = {
-    order: [["id", "DESC"]],
-  };
+  try {
+    // 筛选条件
+    const condition = {
+      order: [["id", "DESC"]],
+    };
 
-  const articles = await Article.findAll(condition);
+    const articles = await Article.findAll(condition);
 
-  res.json({
-    stats: true,
-    message: "数据查询成功",
-    data: {
-      articles,
-    },
-  });
+    res.json({
+      status: 200,
+      message: "数据查询成功",
+      data: {
+        articles,
+      },
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: "数据查询失败",
+      errors: [error.message],
+    });
+  }
 });
 
 module.exports = router;
