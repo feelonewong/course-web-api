1614c401fa36989fe247583af3079671e3bde1bd
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2026-01-17 09:11:57 +0800

    add:完成新建和查询文章

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -9,9 +9,7 @@ router.get("/", async function (req, res) {
     const condition = {
       order: [["id", "DESC"]],
     };
-
     const articles = await Article.findAll(condition);
-
     res.json({
       status: 200,
       message: "数据查询成功",
@@ -28,4 +26,65 @@ router.get("/", async function (req, res) {
   }
 });
 
+// 查询文章列表详情 /admin/articles/:id
+router.get("/:id", async function (req, res) {
+  try {
+    // 筛选条件
+
+    const { id } = req.params;
+    const articles = await Article.findByPk(id);
+
+    if (articles) {
+      res.json({
+        status: 200,
+        message: "数据查询成功",
+        data: {
+          articles,
+        },
+      });
+    } else {
+      res.json({
+        status: 404,
+        message: "数据查询失败",
+        data: {
+          articles,
+        },
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: "数据查询失败",
+      errors: [error.message],
+    });
+  }
+});
+
+// 新建文章 /admin/articles
+router.post("/", async function (req, res) {
+  try {
+    // 筛选条件
+
+    if (!req.body.title) {
+      res.json({
+        status: 200,
+        data: "标题不能为空",
+      });
+    }
+    const articles = await Article.create(req.body);
+    res.json({
+      status: 200,
+      message: "文章创建成功",
+      data: {
+        articles,
+      },
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: "数据查询失败",
+      errors: [error.message],
+    });
+  }
+});
 module.exports = router;
