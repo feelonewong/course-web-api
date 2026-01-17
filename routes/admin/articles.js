6c055892bcfd80ded2fa8be54c2bb80efff432e0
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2026-01-17 09:24:24 +0800

    add:完成删除接口

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -30,7 +30,6 @@ router.get("/", async function (req, res) {
 router.get("/:id", async function (req, res) {
   try {
     // 筛选条件
-
     const { id } = req.params;
     const articles = await Article.findByPk(id);
 
@@ -64,7 +63,6 @@ router.get("/:id", async function (req, res) {
 router.post("/", async function (req, res) {
   try {
     // 筛选条件
-
     if (!req.body.title) {
       res.json({
         status: 200,
@@ -81,6 +79,33 @@ router.post("/", async function (req, res) {
     });
   } catch (error) {
     res.json({
+      status: 500,
+      message: "数据查询失败",
+      errors: [error.message],
+    });
+  }
+});
+
+// 删除 /admin/articles/:id
+router.delete("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    const articles = await Article.findByPk(id);
+    if (articles) {
+      await articles.destroy();
+
+      res.json({
+        status: true,
+        message: "文章删除成功",
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "文章未找到",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
       status: 500,
       message: "数据查询失败",
       errors: [error.message],
